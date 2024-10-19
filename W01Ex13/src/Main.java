148bcd778cc9b5e8215838da292353d37ea7ebab
CuongTang5 <161926271+CuongTang5@users.noreply.github.com>
2024-10-19 15:42:01 +0700

    Add files via upload

---
+++ W01Ex13/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i;
+        for(i=7; i<100; i+=7){
+            sum += 7;
+        }
+    }
+
+        System.out.println("Hello and welcome!", += sum);
+}
